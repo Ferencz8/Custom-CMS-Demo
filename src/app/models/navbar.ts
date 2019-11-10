e29252b524abd7b19be21f0aba1099fb486935d4
Feri <ferenczv8@gmail.com>
2019-11-10 11:21:58 +0200

    Replaced publicsite navbar with backoffice(bo) navbars

--- src/app/models/navbar.ts
+++ src/app/models/navbar.ts
@@ -1,7 +1,17 @@
 export class Navbar {
+
   id: number;
   name: string;
   url: string;
   isPublished: boolean;
   displayOrder: number;
+
+  constructor(params: Navbar = {} as Navbar) {
+
+    this.id = params.id;
+    this.name = params.name;
+    this.url = params.url;
+    this.isPublished = params.isPublished;
+    this.displayOrder = params.displayOrder;
+  }
 }
--- src/app/publicsite/publicsite.component.ts
+++ src/app/publicsite/publicsite.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Navbar } from '../models/navbar';
 
 @Component({
   selector: 'app-publicsite',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicsiteComponent implements OnInit {
 
-  constructor() { }
+  navbars$: Observable<Navbar[]>;
+
+  constructor() {
+
+    this.navbars$ = of([
+      new Navbar({ id: 1, name: 'Home', url: '#home', isPublished: true, displayOrder: 0 }),
+      new Navbar({ id: 2, name: 'About', url: '#about', isPublished: true, displayOrder:  0}),
+      new Navbar({ id: 3, name: 'Services', url: '#services', isPublished: true, displayOrder: 0}),
+      new Navbar({ id: 4, name: 'Team', url: '#team', isPublished: true, displayOrder: 0}),
+      new Navbar({ id: 5, name: 'Portfolio', url: '#portfolio', isPublished: true, displayOrder: 0}),
+      new Navbar({ id: 6, name: 'Blog', url: '#blog', isPublished: true, displayOrder: 0}),
+      new Navbar({ id: 7, name: 'Pricing Tables', url: '#pricing', isPublished: true, displayOrder: 0}),
+      new Navbar({ id: 8, name: 'Contact', url: '#contact', isPublished: true, displayOrder: 0}),
+    ]);
+  }
 
   ngOnInit() {
   }
